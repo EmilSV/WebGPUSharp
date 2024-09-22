35c86b597344ecfb83a1462313ebec0919f48004
Emil Vad <emilsvad@gmail.com>
2024-09-22 21:14:37 +0200

    fix   Required Members

--- src/Color.cs
+++ src/Color.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace WebGpuSharp;
 
 public partial struct Color
 {
+    [SetsRequiredMembers]
     public Color(double r, double g, double b, double a = 1)
     {
         R = r;
--- src/Extent3D.cs
+++ src/Extent3D.cs
@@ -1,10 +1,10 @@
-using System;
-using System.Runtime.InteropServices;
+using System.Diagnostics.CodeAnalysis;
 
 namespace WebGpuSharp;
 
 public partial struct Extent3D
 {
+    [SetsRequiredMembers]
     public Extent3D(uint width, uint height = 1, uint depthOrArrayLayers = 1)
     {
         Width = width;
