966947fbbacbb0064003ae8481264f0ed08f047c
Emil Vad <emilsvad@gmail.com>
2024-10-15 23:39:24 +0200

    added GetCompilationInfoAsync

--- src/CompilationInfoCallback.cs
+++ src/CompilationInfoCallback.cs
@@ -4,3 +4,9 @@ public delegate void CompilationInfoCallback(
     CompilationInfoRequestStatus status,
     CompilationInfo info
 );
+
+public delegate T CompilationInfoCallback<T>(
+    CompilationInfoRequestStatus status,
+    CompilationInfo info
+);
+
--- src/internal/ShaderModuleGetCompilationInfoHandler.cs
+++ src/internal/ShaderModuleGetCompilationInfoHandler.cs
@@ -13,6 +13,44 @@ public unsafe static class ShaderModuleGetCompilationInfoHandler
         WebGPU_FFI.ShaderModuleGetCompilationInfo(handle, &OnCallback, (void*)callbackHandle);
     }
 
+
+    public static Task<T> GetCompilationInfoAsync<T>(ShaderModuleHandle handle, CompilationInfoCallback<T> callback)
+    {
+        TaskCompletionSource<T> tcs = new();
+        void innerCallback(CompilationInfoRequestStatus status, CompilationInfo info)
+        {
+            try
+            {
+                tcs.SetResult(callback(status, info));
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        }
+        GetCompilationInfo(handle, innerCallback);
+        return tcs.Task;
+    }
+
+    public static Task GetCompilationInfoAsync(ShaderModuleHandle handle, CompilationInfoCallback callback)
+    {
+        TaskCompletionSource tcs = new();
+        void innerCallback(CompilationInfoRequestStatus status, CompilationInfo info)
+        {
+            try
+            {
+                callback(status, info);
+                tcs.SetResult();
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+        }
+        GetCompilationInfo(handle, innerCallback);
+        return tcs.Task;
+    }
+
     [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
     public static void OnCallback(
         CompilationInfoRequestStatus status, CompilationInfoFFI* compilationInfo, void* userdata)
