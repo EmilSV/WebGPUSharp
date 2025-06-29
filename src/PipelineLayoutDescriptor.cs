23c048569f48f1b389fe5daa6c87aa113761899f
Emil Vad <emilsvad@gmail.com>
2025-06-29 21:16:09 +0200

    fixed wrong default for ImmediateSize

--- src/PipelineLayoutDescriptor.cs
+++ src/PipelineLayoutDescriptor.cs
@@ -10,7 +10,7 @@ public ref partial struct PipelineLayoutDescriptor
     /// <inheritdoc cref ="PipelineLayoutDescriptorFFI.BindGroupLayouts"/>
     public required ReadOnlySpan<BindGroupLayout> BindGroupLayouts;
 
-    public uint ImmediateSize = WebGPU_FFI.LIMIT_U32_UNDEFINED;
+    public uint ImmediateSize = 0;
 
     public PipelineLayoutDescriptor()
     {
