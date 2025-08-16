30c082a69d4eb6c1693699779ba744925381df21
Emil Vad <emilsvad@gmail.com>
2025-08-16 21:26:20 +0200

    added BuiltIn allocator

--- src/marshalling/WebGpuMarhallingMemory.cs
+++ src/marshalling/WebGpuMarhallingMemory.cs
@@ -1,3 +1,6 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 namespace WebGpuSharp.Marshalling;
 
 public static unsafe class WebGpuMarshallingMemory
@@ -7,10 +10,19 @@ public static unsafe class WebGpuMarshallingMemory
         public WebGpuAllocator* Allocator;
     }
 
+    private static int _isBuiltInAllocatorInitialized = 0;
+    private readonly static Lazy<WebGpuAllocatorPtr> _builtinAllocator = new(() =>
+    {
+        var allocator = (WebGpuAllocator*)NativeMemory.AllocZeroed(1, (nuint)sizeof(WebGpuAllocator));
+        ref var allocatorRef = ref *allocator;
+        allocatorRef = BuiltInWebGPUAllocator.GetWebGpuAllocator();
+        return new WebGpuAllocatorPtr { Allocator = allocator };
+
+    }, isThreadSafe: true);
     private static WebGpuAllocator* _defaultAllocator;
     private static readonly AsyncLocal<WebGpuAllocatorPtr> _asyncLocalAllocator = new AsyncLocal<WebGpuAllocatorPtr>();
 
-    internal static void SetDefaultAllocator(WebGpuAllocator* allocator)
+    public static void SetDefaultAllocator(WebGpuAllocator* allocator)
     {
         _defaultAllocator = allocator;
     }
@@ -20,18 +32,21 @@ public static unsafe class WebGpuMarshallingMemory
         _asyncLocalAllocator.Value = new WebGpuAllocatorPtr { Allocator = allocator };
     }
 
-
     public const int DefaultStartStackSize = 32;
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static WebGpuAllocatorHandle GetAllocatorHandle(void* stackMemory, nuint size)
     {
-        WebGpuAllocator* allocator = _defaultAllocator;
+        WebGpuAllocator* allocator = _asyncLocalAllocator.Value.Allocator;
         if (allocator == null)
         {
-            allocator = _asyncLocalAllocator.Value.Allocator;
+            allocator = _defaultAllocator;
+        }
+        if (allocator == null)
+        {
+            _defaultAllocator = allocator = _asyncLocalAllocator.Value.Allocator;
         }
 
         return new WebGpuAllocatorHandle(allocator, stackMemory, size);
     }
-
 }
